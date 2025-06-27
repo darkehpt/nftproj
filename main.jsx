d50fef44111678b59050011652ebd9f2446ce23d
Francisco Matos <franciscomatos@Franciscos-MacBook-Air.local>
2025-06-27 15:12:20 +0100

    final25backend3.16

--- main.jsx
+++ main.jsx
@@ -1,15 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import { WalletConnectionProvider } from "./WalletConnectionProvider";
+import AppWrapper from "./App"; // Import the wrapper, not the bare App
 import "@solana/wallet-adapter-react-ui/styles.css";
-import { Buffer } from 'buffer'
-window.Buffer = Buffer
+import { Buffer } from "buffer";
+window.Buffer = Buffer;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <WalletConnectionProvider>
-      <App />
-    </WalletConnectionProvider>
+    <AppWrapper />
   </React.StrictMode>
 );
